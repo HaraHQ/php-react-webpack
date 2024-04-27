37c7cc6c8ba7a2fe87a8e5d8f9298a70785f410e
Muhammad Rizkiansyah <18541319-harahq@users.noreply.gitlab.com>
2024-04-28 00:36:49 +0700

    fix: increase version

--- src/template.php
+++ src/template.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>React App with PHP!</title>
+    <title>React 18 + PHP + Webpack</title>
 </head>
 <body>
     <div id="react-app"></div>
