793b1e139466b760d540724e6a3ed390211b31ea
Muhammad Rizkiansyah <18541319-harahq@users.noreply.gitlab.com>
2024-04-27 23:45:49 +0700

    fix: webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,56 +2,60 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'scripts/[name].bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader, // Extract CSS into files
-          'css-loader' // Processes CSS
-        ]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-typescript']
+module.exports = (env: any, argv: any) => {
+  const isDevelopment = argv.mode !== 'production';
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'scripts/[name].bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader, // Extract CSS into files
+            'css-loader' // Processes CSS
+          ]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-typescript']
+            }
           }
         }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx']
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css' // Output CSS to css folder
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/template.php',
-      filename: 'index.php',
-      inject: true,
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+      ]
     },
-    compress: true,
-    port: 3000, // You can change this port
-  }
-};
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx']
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css' // Output CSS to css folder
+      }),
+      new HtmlWebpackPlugin({
+        template: 'src/template.php',
+        filename: isDevelopment ? 'index.html' : 'index.php',
+        inject: true,
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 3000, // You can change this port
+    }
+  };
+}
