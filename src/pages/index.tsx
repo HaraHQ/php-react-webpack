62f0a01711b6b601e88348957b5be89dd2f27b4c
Rixki <107851039+HaraHQ@users.noreply.github.com>
2024-04-29 12:22:18 +0700

    chore: add google font example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Home: FC = () => {
     return (
-        <div className="text-2xl font-semibold text-blue-500">Home</div>
+        <div className="text-2xl font-semibold text-blue-500">Wow it's Home</div>
     )
 }
 
